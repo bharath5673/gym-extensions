d5e4cfd11a1026f33a4c5788ae506a41ef340623
Victor Barbaros <barbarosvictor@yahoo.com>
2017-07-10 21:57:22 -0400

    wrote some tests for the contextual envs

--- tests/test_contextual_envs.py
+++ tests/test_contextual_envs.py
@@ -1,26 +1,69 @@
 import gym
 import gym_extensions
+from gym.envs.classic_control.cartpole import CartPoleEnv
+from gym.envs.classic_control.pendulum import PendulumEnv
 
-env = gym.make('CartPoleContextual-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
-    print env.unwrapped.context_space_info()
-    context_vect = [0.1, 0.1, 0.1, 0.1]
-    print 'context before: ', env.unwrapped.context 
-    env.unwrapped.change_context(context_vect)
-    print 'context after:  ', env.unwrapped.context
+
+def test_cartpole_contextual(env_id):
+    env = gym.make(env_id)
+    if isinstance(env.unwrapped, CartPoleEnv):
+        env.reset()
+    else:
+    	raise NotImplementedError
+
+    if 7 == len(env.unwrapped.context_space_info().keys()):
+    	pass
+    else:
+    	print 'context_space_info() function needs to be implemented!'
+        raise NotImplementedError
     
+    context_vect = [0.01, 0.01, 0.01, 0.01]
 
+    if context_vect != env.unwrapped.context:
+    	pass
+    else:
+        raise AttributeError 
 
-env = gym.make('PendulumContextual-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
-    print env.unwrapped.context_space_info()
-    context_vect = [0.1, 0.1]
-    print 'context before: ', env.unwrapped.context 
     env.unwrapped.change_context(context_vect)
-    print 'context after:  ', env.unwrapped.context
+
+    if context_vect == env.unwrapped.context:
+    	pass
+    else:
+    	raise AttributeError
+
+
+def test_pendulum_contextual(env_id):
+    env = gym.make(env_id)
+    if isinstance(env.unwrapped, PendulumEnv):
+        env.reset()
+    else:
+    	raise NotImplementedError
+
+    if 7 == len(env.unwrapped.context_space_info().keys()):
+    	pass
+    else:
+    	print 'context_space_info() function needs to be implemented!'
+        raise NotImplementedError
+    
+    context_vect = [0.01, 0.01]
+
+    if context_vect != env.unwrapped.context:
+    	pass
+    else:
+        raise AttributeError 
+
+    env.unwrapped.change_context(context_vect)
+
+    if context_vect == env.unwrapped.context:
+    	pass
+    else:
+    	raise AttributeError
+
+
+
+if __name__=="__main__":
+	env_id_cartpole = 'CartPoleContextual-v0'
+	test_cartpole_contextual(env_id_cartpole)
+
+	env_id_pendulum = 'PendulumContextual-v0'
+	test_pendulum_contextual(env_id_pendulum)
